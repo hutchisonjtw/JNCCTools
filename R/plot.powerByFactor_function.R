190bcdd636b78319ea4e6f548edd7a7a9a9fa0ef
James Hutchison <james.hutchison@jncc.gov.uk>
2017-02-13 17:43:13 +0000

    Add target power to plot method for red line instead of alpha.

--- R/plot.powerByFactor_function.R
+++ R/plot.powerByFactor_function.R
@@ -7,8 +7,8 @@ plot.powerByFactor <- function(x, y, ...) {
   par(mfrow = c(panelRows, panelCols))
   plotWithLine <- function(x, y, main, alpha) {
     plot(x, y, main = main, xlim = c(0, max(x)), ylim = c(0, 1), xlab = "n samples", ylab = "Proportion significant")
-    lines(x = c(-100, max(x)*2), y = c(1-alpha, 1-alpha), col = "red")
+    lines(x = c(-100, max(x)*2), y = c(target.power, target.power), col = "red")
   }
-  invisible(mapply(plotWithLine, y = x$result, main = titles, MoreArgs = list(x = x$nSample, alpha = x$alpha)))
+  invisible(mapply(plotWithLine, y = x$result, main = titles, MoreArgs = list(x = x$nSample, target.power = x$target.power)))
   par(mfrow = c(1, 1))
 }
