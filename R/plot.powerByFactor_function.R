857b09b14075476eaccc4e44d859e09022e3d5ec
James Hutchison <james.hutchison@jncc.gov.uk>
2017-03-01 20:42:14 +0000

    Error correction

--- R/plot.powerByFactor_function.R
+++ R/plot.powerByFactor_function.R
@@ -5,7 +5,7 @@ plot.powerByFactor <- function(x, y, ...) {
   panelCols <- round(sqrt(length(titles)))
   panelRows <- ceiling(length(titles)/panelCols)
   par(mfrow = c(panelRows, panelCols))
-  plotWithLine <- function(x, y, main, alpha) {
+  plotWithLine <- function(x, y, main, target.power) {
     plot(x, y, main = main, xlim = c(0, max(x)), ylim = c(0, 1), xlab = "n samples", ylab = "Proportion significant")
     lines(x = c(-100, max(x)*2), y = c(target.power, target.power), col = "red")
   }
